7d016d61cabc3409bc7e7c8f1e307a65b62f1022
mdwenning <63916041+mdwenning@users.noreply.github.com>
2020-04-18 10:50:35 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,103 @@
+var mystery1 = document.getElementById("Mystery1");
+var important1 = document.getElementById("Important1");
+var important2 = document.getElementById("Important2");
+var important3 = document.getElementById("Important3");
+var combat1 = document.getElementById("Combat1");
+var combat2 = document.getElementById("Combat2");
+var combat3 = document.getElementById("Combat3");
+var reward1 = document.getElementById("Reward1");
+var reward2 = document.getElementById("Reward2");
+var death1 = document.getElementById("Death1");
+
+mystery1.onmouseover = function(){
+    document.getElementById("Blank").src = "Images/Mystery.png";
+    document.getElementById("Statements1").innerHTML = "Letter From Alchemist:";
+    document.getElementById("Statements2").innerHTML = "I will get to work immediately on a rocket the likes of which have never been seen before. It will be filled with explosive chemicals to blow this rock out of the sky. Help would be appreciated to get this finished in time…";
+    document.getElementById("Statements3").innerHTML = "Letter From Sorceress:";
+    document.getElementById("Statements4").innerHTML = "If you can find the first half of this ‘Dome-Shield’ spell, I might be able to translate it in time to place a shield that can block the area from the blow of the meteor. Godspeed.";
+    document.getElementById("Statements5").innerHTML = "Piece of Spell Incantation:";
+    document.getElementById("Statements6").innerHTML = "SGHR VHKK OQNAZAKX RTLLNM Z CNLD RGZODC RGHDKC KHJD EZHQKX KZQFD SN OQNSDBS...";
+}
+important1.onmouseover = function(){
+    document.getElementById("Blank").src = "Images/Important.png";
+    document.getElementById("Statements1").innerHTML = "The End is Near!";
+    document.getElementById("Statements2").innerHTML = "A large crowd begins to form centered about some yelling from an individual. You make your way into the crowd to see what is going on. ‘The End is Near!’ - you hear over the chatter. ‘.. Meteor … Very Spot …’ ";
+    document.getElementById("Statements3").innerHTML = "A group from the crowd moves closer to the yelling man. ‘Action … Stop thi-’ The man suddenly stops yelling and the crowd disperses in a rush, some screaming. Leaving the unconscious body of the man on the ground";
+    document.getElementById("Statements4").innerHTML = "";
+    document.getElementById("Statements5").innerHTML = "";
+    document.getElementById("Statements6").innerHTML = "";
+
+}
+important2.onmouseover = function(){
+    document.getElementById("Blank").src = "Images/Important.png";
+    document.getElementById("Statements1").innerHTML = "";
+    document.getElementById("Statements2").innerHTML = "";
+    document.getElementById("Statements3").innerHTML = "";
+    document.getElementById("Statements4").innerHTML = "";
+    document.getElementById("Statements5").innerHTML = "";
+    document.getElementById("Statements6").innerHTML = "";
+}
+important3.onmouseover = function(){
+    document.getElementById("Blank").src = "Images/Important.png";
+    document.getElementById("Statements1").innerHTML = "";
+    document.getElementById("Statements2").innerHTML = "";
+    document.getElementById("Statements3").innerHTML = "";
+    document.getElementById("Statements4").innerHTML = "";
+    document.getElementById("Statements5").innerHTML = "";
+    document.getElementById("Statements6").innerHTML = "";
+}
+combat1.onmouseover = function(){
+    document.getElementById("Blank").src = "Images/Combat.png";
+    document.getElementById("Statements1").innerHTML = "";
+    document.getElementById("Statements2").innerHTML = "";
+    document.getElementById("Statements3").innerHTML = "";
+    document.getElementById("Statements4").innerHTML = "";
+    document.getElementById("Statements5").innerHTML = "";
+    document.getElementById("Statements6").innerHTML = "";
+}
+combat2.onmouseover = function(){
+    document.getElementById("Blank").src = "Images/Combat.png";
+    document.getElementById("Statements1").innerHTML = "";
+    document.getElementById("Statements2").innerHTML = "";
+    document.getElementById("Statements3").innerHTML = "";
+    document.getElementById("Statements4").innerHTML = "";
+    document.getElementById("Statements5").innerHTML = "";
+    document.getElementById("Statements6").innerHTML = "";
+}
+combat3.onmouseover = function(){
+    document.getElementById("Blank").src = "Images/Combat.png";
+    document.getElementById("Statements1").innerHTML = "";
+    document.getElementById("Statements2").innerHTML = "";
+    document.getElementById("Statements3").innerHTML = "";
+    document.getElementById("Statements4").innerHTML = "";
+    document.getElementById("Statements5").innerHTML = "";
+    document.getElementById("Statements6").innerHTML = "";
+}
+reward1.onmouseover = function(){
+    document.getElementById("Blank").src = "Images/Reward.png";
+    document.getElementById("Statements1").innerHTML = "";
+    document.getElementById("Statements2").innerHTML = "";
+    document.getElementById("Statements3").innerHTML = "";
+    document.getElementById("Statements4").innerHTML = "";
+    document.getElementById("Statements5").innerHTML = "";
+    document.getElementById("Statements6").innerHTML = "";
+}
+reward2.onmouseover = function(){
+    document.getElementById("Blank").src = "Images/Reward.png";
+    document.getElementById("Statements1").innerHTML = "";
+    document.getElementById("Statements2").innerHTML = "";
+    document.getElementById("Statements3").innerHTML = "";
+    document.getElementById("Statements4").innerHTML = "";
+    document.getElementById("Statements5").innerHTML = "";
+    document.getElementById("Statements6").innerHTML = "";
+}
+death1.onmouseover = function(){
+    document.getElementById("Blank").src = "Images/Death.png";
+    document.getElementById("Statements1").innerHTML = "";
+    document.getElementById("Statements2").innerHTML = "";
+    document.getElementById("Statements3").innerHTML = "";
+    document.getElementById("Statements4").innerHTML = "";
+    document.getElementById("Statements5").innerHTML = "";
+    document.getElementById("Statements6").innerHTML = "";
+}
+
